a84b003e7dea944a380d513ddaa59a84f78906f5
Iago Anjos <iagokaued@gmail.com>
2025-07-29 19:27:11 -0300

    testando css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from "./assets/components/Heading";
+import "./styles/global.css";
+import "./styles/theme.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  console.log("testando");
 
   return (
     <>
+      <Heading />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam enim
+        perferendis provident neque quis mollitia tempora est, voluptate in. Eos
+        odit distinctio molestiae quisquam molestias, suscipit possimus corrupti
+        consequatur ut.
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
