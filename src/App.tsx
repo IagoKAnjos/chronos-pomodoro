50fc24f0d429fef64339dae812449c1f8551d1de
Iago Anjos <iagokaued@gmail.com>
2025-07-30 19:19:08 -0300

    criando modulos index e style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { Heading } from "./assets/components/Heading";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
   return (
     <>
-      <Heading> Teste 1</Heading>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam enim
-        perferendis provident neque quis mollitia tempora est, voluptate in. Eos
-        odit distinctio molestiae quisquam molestias, suscipit possimus corrupti
-        consequatur ut.
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
